9a83740ea3ffa80f1b66192b3b5e039a8c8323f5
blixuk <blixuk@gmail.com>
2021-12-08 16:04:14 +0000

    added list functions

--- config.py
+++ config.py
@@ -3,6 +3,7 @@
 import os
 from configparser import RawConfigParser
 
+import style
 from directory import Directory
 
 class Config:
@@ -22,6 +23,20 @@ class Config:
 	def save(self):
 		with open(Config.config_path, 'w') as config:
 			Config.config.write(config)
+
+	def list(self) -> list:
+		sections:list = []
+		for section in Config.config.sections():
+			sections.append(f"[{style.yellow(section)}]")
+		return sections
+
+	def list_full(self) -> list:
+		sections:list = []
+		for section in Config.config.sections():
+			sections.append(f"[{style.yellow(section)}]")
+			for option in Config.config.options(section):
+				sections.append(f"\t{style.green(option)}")
+		return sections
 
 	def make_config(self):
 
