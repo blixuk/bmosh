07b95fb60a58e6183cb28f1f34709833df16352a
blixuk <blixuk@gmail.com>
2021-12-10 16:30:57 +0000

    code clean up and debugging added

--- shell.py
+++ shell.py
@@ -1,11 +1,20 @@
 # file: bmo/shell.py
 
+from os import dup
+import sys
+from typing import final
+
+import style
+from config import Config
 from prompt import Prompt
 from parser import Parser
 from session import Session
-from logging import Log
-from config import Config
 from alias import Alias
+from history import History
+from logging import Log
+
+debug = Log().debug
+log = Log().log
 
 class Shell:
 
@@ -17,39 +26,57 @@ class Shell:
 		for count, _ in enumerate(iter(bool, True)):
 			try:
 				line = input(Prompt().prompt(count))
+				debug("loop", str(line))
 				self.pre_loop(count, line)
 			except KeyboardInterrupt:
-				self.on_exit()
-				break
+				debug("KeyboardInterrupt", "exit clean!")
+				self.exit_handler("KeyboardInterrupt")
 			except EOFError:
-				self.on_exit()
-				break
+				debug("EOFError")
+			except SystemExit:
+				debug("SystemExit", "exit clean!")
+				self.on_exit("SystemExit")
 
 	def pre_loop(self, count:int, line:str) -> any:
+		debug("pre_Loop_in", str(line))
+		History().add(line)
 		line = Parser().parse(line)
 		if line is not None:
 			self.main_loop(count, line)
-		return count, line
+		debug("pre_Loop_out", str(line))
 
 	def main_loop(self, count:int, line:str) -> any:
+		debug("main_Loop_in", str(line))
 		if line is not None:
 			self.post_loop(count, line)
 		else:
-			Log().log("main_Loop", line)
-		return count, line
+			log("main_Loop", str(line))
+		debug("main_Loop_out", str(line))
 
 	def post_loop(self, count:int, line:str) -> any:
+		debug("post_Loop_in", str(line))
 		if line is not None:
 			print(line)
 		else:
-			Log().log("post_Loop", line)
-		return count, line
+			log("post_Loop", str(line))
+		debug("post_Loop_out", str(line))
 
 	def on_start(self) -> None:
 		Config().load()
 		Alias().load()
+		History().load()
 		Session().load()
 		print('\033[H\033[J')
 
-	def on_exit(self) -> None:
+	def on_exit(self, message:str="") -> None:
 		Config().save()
+		Alias().save()
+		History().save()
+		Session().save()
+		log("exit", message)
+		sys.exit(0)
+
+	def exit_handler(self, message:str) -> None:
+		responce = input(style.red("Do you want to exit? [y/n] "))
+		if responce in ["y", "Y", "yes", "Yes", "YES"]:
+			self.on_exit(message)
