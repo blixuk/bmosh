70ee8072c42c902602f3172d09fd409b2c301573
blixuk <blixuk@gmail.com>
2021-12-13 17:31:17 +0000

    add help function

--- commands/clear.py
+++ commands/clear.py
@@ -1,14 +1,20 @@
 # file: bmo/commands/clear.py
 
+import style
+
 class Comamnd:
-	'''
-clear the terminal window\n
-defualt:
-	clear:\t\t clear the terminal window
-	'''
 
 	def default(self, args:list) -> None:
 		print("\033[H\033[J")
+		return None
+
+	def help(self, args:list) -> None:
+		help = {
+			"name": "clear",
+			"description": "clear the terminal window",
+			"default": {"clear" : "clear the terminal window"},
+		}
+		print(style.help(help))
 		return None
 
 def run() -> str:
